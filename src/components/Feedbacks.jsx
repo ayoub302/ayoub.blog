f0f32f9abebe3eb58e4b7df60a62982856e27f65
ayoub302 <ayoub2004.bensaid.123@gmail.com>
2025-01-29 14:37:10 +0100

    hello

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
-import { styles } from "../../styles";
-import { SectionWrapper } from "../../hoc";
-import { fadeIn, textVariant } from "../../utils/motion";
-import { testimonials } from "../../constants";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
 
 const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => {
     return (
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,10 +1,10 @@
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-import { styles } from "../../styles";
+import { styles } from "../styles";
 import { github } from "../assets";
-import { SectionWrapper } from "../../hoc";
-import { projects } from "../../constants";
-import { fadeIn, textVariant } from "../../utils/motion";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
